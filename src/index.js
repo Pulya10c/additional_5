8d6c08dbc3bc3f5c89bf0da764b792a630b0184b
Pulya10c <sergienya.oleg@yandex.ru>
2018-09-21 11:46:42 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-
-  if(typeof str !== 'string')
+  
+  if (str.length %2 ) {
     return false;
+  }
 
-  let stack = [],
-      // Сюда можно добавлять свои скобки: ключ - открываемая, значение - закрываемая
-      brackets = {},
-      openRegExp = [],
-      closeRegExp = [],
-      str1 = str;
-     
-      function ObgBreak (a,b) {
-   
-        brackets[a] = b;
-        
-      };
-      
-      for (let j=0;j<bracketsConfig.length;j++) {
-          ObgBreak (bracketsConfig[j][0],bracketsConfig[j][1]);
-      };
-  
-  // Создаём регулярные выражения для поиска
-  Object.keys(brackets).forEach(e => openRegExp.push(`\\${e}`));
-  openRegExp = new RegExp(openRegExp.join('|'));
-  for(let i in brackets) if(brackets.hasOwnProperty(i)) closeRegExp.push(`\\${brackets[i]}`);
-  closeRegExp = new RegExp(closeRegExp.join('|'));
-  
-  // Добавляем все найденные открывающие скобки в стёк
-  while((tmp = openRegExp.exec(str1)) && (str1 = str1.substr(++tmp.index))) stack.push(tmp[0]);
-  
-  str1 = str;
 
-  // Если нашли какую-то закрывающую скобку
-  while((tmp = closeRegExp.exec(str1)) && (str1 = str1.substr(++tmp.index)))
-    // То проверяем: Или закончился стёк, а закрывающие скобки всё прибывают
-    // Или является ли найденная скобка парой к открывающей скобке в конце стёка (самой глубокой)
-    if(!stack.length || brackets[stack.pop()] !== tmp[0]) return false;
+  const {open, close} = bracketsConfig.reduce((acc, curr) => {
+      acc.open.push(curr[0]);
+      acc.close.push(curr[1]);
+      return acc;
+  }, {open:[], close:[]});
+
+/*
+  let open = [];
+  let close = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++){
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+ */
+  const stack =[];
+
+  for (let i=0; i<str.length ;i++) {
+    let index = close.findIndex(s=>s===str[i]);
+    let openSymbol = open[index];
+
+    let flag = true;
+      if (close.includes(str[i])) {
+        if (stack[stack.length-1]===openSymbol){ 
+          stack.pop();
+          flag = false;
+      }
+    }
+    if (open.includes(str[i]) && flag) {
+      stack.push(str[i]);
+    }
+    
+  }
   
-  // Всё ОК
-  return true; 
+ if (stack.length !== 0){
+    return false;
+  }
+  return true;
     
 }
