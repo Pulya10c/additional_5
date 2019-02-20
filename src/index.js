62bb61b8e5844e2bc3f0b3181eaf3d63c6243000
Aleh Serhiyenia <sergienya.oleg@yandex.ru>
2019-02-21 00:16:20 +0300

    style: add function es6

--- src/index.js
+++ src/index.js
@@ -4,40 +4,30 @@ module.exports = function check(str, bracketsConfig) {
     return false;
   }
 
-
   const {open, close} = bracketsConfig.reduce((acc, curr) => {
       acc.open.push(curr[0]);
       acc.close.push(curr[1]);
       return acc;
   }, {open:[], close:[]});
 
-/*
-  let open = [];
-  let close = [];
-
-  for (let i = 0; i < bracketsConfig.length; i++){
-    open.push(bracketsConfig[i][0]);
-    close.push(bracketsConfig[i][1]);
-  }
- */
   const stack =[];
 
-  for (let i=0; i<str.length ;i++) {
-    let index = close.findIndex(s=>s===str[i]);
+  [...str].forEach((el, idx)=>{
+    let index = close.findIndex(s=>s===el);
     let openSymbol = open[index];
-
     let flag = true;
-      if (close.includes(str[i])) {
+      if (close.includes(el)) {
         if (stack[stack.length-1]===openSymbol){ 
           stack.pop();
           flag = false;
       }
     }
-    if (open.includes(str[i]) && flag) {
-      stack.push(str[i]);
+    if (open.includes(el) && flag) {
+      stack.push(el);
     }
-    
-  }
+  });
+  
+
   
  if (stack.length !== 0){
     return false;
@@ -45,3 +35,24 @@ module.exports = function check(str, bracketsConfig) {
   return true;
     
 }
+
+// module.exports = function check(str, bracketsConfig) {
+//   console.log(bracketsConfig);
+ 
+//   const res = [];
+ 
+//   [...str].forEach(el => {
+//    bracketsConfig.forEach((elem, i) => {
+//      let flag = true;
+//     if (elem[0] === el && elem[0] !== elem[1]) {
+//      res.push(i);
+//      if (elem[0] === elem[1]) flag = false;
+//     } else if (elem[1] === el && res[res.length - 1] === i ) {
+//      res.pop();
+//     }
+//    });
+//   });
+//   console.log(str);
+//   console.log(res);
+//   return !res.length;
+//  };
